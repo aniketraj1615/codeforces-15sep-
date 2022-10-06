939d963c5512a78afe4e7c202c3d932d4eb4a4d0
aniketraj1615 <aniketraj1615@gmail.com>
2022-10-06 23:56:44 +0530

    m

--- id51.cpp
+++ id51.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main(){
     int n,a,b;
     cin>>n>>a>>b;
-  int pos=(n-a);
-  cout<<pos<<endl;
+    cout<<n  <<";"<<a<<"  "<<b<<endl;
+    cout<<n-a<<endl;
+  int position=(n-a);
+//   cout<<pos<<endl;
   if(b==0){
    cout<<"1"<<endl;
   }
@@ -12,11 +14,11 @@ int main(){
 
   
  
-  while(pos>b){
-   cout<<pos<<endl;
-   pos--;
+  while(position>b){
+   // cout<<pos<<endl;
+   position--;
   }
-  cout<<pos<<endl;
+  cout<<position<<endl;
   }
 return 0;
 }
